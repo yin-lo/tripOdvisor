12764900834d0b8dc527b0763cea2bb7001c87ae
yin-lo <yinlo@hotmail.fr>
2024-02-21 09:25:52 +0100

    exemple de fonctions fléchées

---
+++ js/correction.js
@@ -0,0 +1,12 @@
+/* ------------------------------------------- */
+/* ------------exemple de fonctions fléchées--- */
+/* ------------------------------------------- */
+
+/* const hello = () => {
+	console.log(hello);
+} */
+
+/* si la fonction n'a qu'un paramètre et ne retourne qu'une instruction, on peut écrire : */
+/* const double = (number) => number * 2;
+console.log(double(5)); */
+
